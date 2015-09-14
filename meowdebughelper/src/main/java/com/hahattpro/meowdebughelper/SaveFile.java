7c04e63d77f489473ee6f959529e2fdd305c1a91
thien.thai <tthien@apcs.vn>
2015-09-14 11:43:27 +0700

    savefile static method

--- meowdebughelper/src/main/java/com/hahattpro/meowdebughelper/SaveFile.java
+++ meowdebughelper/src/main/java/com/hahattpro/meowdebughelper/SaveFile.java
@@ -8,6 +8,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 
 
@@ -45,6 +47,59 @@ public class SaveFile {
             //do nothing
         }
         Log.i("SaveFile path","path : "+path);
+    }
+
+    public static File save(String NAME, String file_body) {
+        String FILE_NAME =NAME;
+
+        String path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)+ "/" +FILE_NAME;
+        File file = null;
+        try{
+            file = new File(path);
+            FileOutputStream out = new FileOutputStream(file);
+            OutputStreamWriter writer = new OutputStreamWriter(out);
+            writer.write(file_body);
+            writer.close();
+            out.close();
+        }
+        catch (FileNotFoundException e){
+
+        }
+        catch (IOException e){
+            //do nothing
+        }
+        Log.i("SaveFile path","path : "+path);
+        return file;
+    }
+
+    private static File save(String NAME, InputStream file_body){
+        File file= null;
+
+        try{
+            file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),NAME);
+            copy(file_body, file);
+        }
+        catch (FileNotFoundException e){
+
+        }
+        catch (IOException e){
+            //do nothing
+        }
+        return file;
+    }
+
+    public static void copy(InputStream in, File dst) throws IOException {
+
+        OutputStream out = new FileOutputStream(dst);
+
+        // Transfer bytes from in to out
+        byte[] buf = new byte[1024];
+        int len;
+        while ((len = in.read(buf)) > 0) {
+            out.write(buf, 0, len);
+        }
+        in.close();
+        out.close();
     }
 
     public File getFile()
